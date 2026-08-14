31706073a8082b8b5da27200caf683fd648a511f
Khushi-Kumari006 <singhkhushi6877@gmail.com>
2026-08-14 13:39:34 +0530

    Time: 1 ms (99%), Space: 45.1 MB (78.81%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
@@ -1,12 +1,16 @@
 class Solution {
     public int countKDifference(int[] nums, int k) {
-         int count = 0;
-         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (Math.abs(nums[i] - nums[j]) == k) {
-                    count++;
-                }
+         int[] freq = new int[101];
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (num - k >= 1) {
+                count += freq[num - k];
             }
+            if (num + k <= 100) {
+                count += freq[num + k];
+            }
+            freq[num]++;
         }
         return count;
         
