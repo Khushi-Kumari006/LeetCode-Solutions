c3f2da2c3278c4e9c94a9959b775d5de2ff1648f
Khushi-Kumari006 <singhkhushi6877@gmail.com>
2026-08-21 08:27:58 +0530

    Time: 7 ms (100%), Space: 43.9 MB (68.63%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.java
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.java
@@ -1,59 +1,44 @@
 class Solution {
-    public long findKthSmallest(int[] coins, int k) {
-        long low = 1;
-        long high = (long) coins[0] * k;
-        for (int coin : coins) {
-            high = Math.min(high, (long) coin * k);
-        }
-        while (low < high) {
-            long mid = low + (high - low) / 2;
-            if (count(mid, coins) >= k) {
-                high = mid;
-            } else {
-                low = mid + 1;
+    public long findKthSmallest(int[] c, int k) {
+        Arrays.sort(c);
+        if(c[0]==1)return k;
+        long[] lcm = new long[(1<<c.length)];
+        boolean[] odd = new boolean[lcm.length];
+        for(int i=0; i<c.length; i++)lcm[1<<i]=c[i];
+        for(int i=1; i<lcm.length; i++){
+            odd[i]=odd(i);
+            for(int j=0; lcm[i]==0&&j<c.length; j++){
+                if((i&(1<<j))>0)lcm[i]=lcm(lcm[i-(1<<j)], c[j]);
             }
         }
-        return low;
-    }
-private long count(long x, int[] coins) {
-        int n = coins.length;
-        long total = 0;
-        for (int mask = 1; mask < (1 << n); mask++) {
-            long lcm = 1;
-            int bits = 0;
-            boolean valid = true;
-            for (int i = 0; i < n; i++) {
-                if ((mask & (1 << i)) != 0) {
-                    bits++;
-                    lcm = getLCM(lcm, coins[i]);
-                    if (lcm > x) {
-                        valid = false;
-                        break;
-                    }
-                }
-            }
-            if (!valid) {
-                continue;
-            }
-            long multiples = x / lcm;
-            if (bits % 2 == 1) {
-                total += multiples;
-            } else {
-                total -= multiples;
-            }
+        long l=k, r=(long)c[c.length-1]*k, m;
+        while(l<r){
+            m=(l+r)/2;
+            if(count(m, odd, lcm)<k)l=m+1;
+            else r=m;
         }
-        return total;
+        return l;
     }
-private long getLCM(long a, long b) {
-        long gcd = getGCD(a, b);
-        return a / gcd * b;
-    }
-private long getGCD(long a, long b) {
-        while (b != 0) {
-            long temp = a % b;
-            a = b;
-            b = temp;
+    private long count(long x, boolean[] odd, long[] lcm){
+        long a=0;
+        for(int i=1; i<lcm.length; i++){
+            if(odd[i])a+=x/lcm[i];
+            else a-=x/lcm[i];
         }
         return a;
+    }
+    private long lcm(long a, long b){
+        return a*b/gcd(a, b);
+    }
+    private long gcd(long a, long b){
+        if(b%a==0)return a;
+        return gcd(b%a, a);
+    }
+    private boolean odd(int x){
+        boolean odd = false;
+        for(; x>0; x>>=1){
+            if(x%2==1)odd=!odd;
+        }
+        return odd;
     }
 }
