54444c12473aa4ff1511aec2641293701a259dc4
Khushi-Kumari006 <singhkhushi6877@gmail.com>
2026-08-24 10:05:31 +0530

    Time: 74 ms (66.94%), Space: 158.4 MB (80.08%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,15 +1,21 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        Map<Integer, Integer> losses = new TreeMap<>();
-        for(int[] m : matches){
-            losses.put(m[0], losses.getOrDefault(m[0], 0));
-            losses.put(m[1], losses.getOrDefault(m[1], 0) + 1);
+        List<List<Integer>> ans = new ArrayList<>();
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < matches.length; i++) {
+            hm.put(matches[i][1], hm.getOrDefault(matches[i][1], 0) + 1);
+            hm.put(matches[i][0], hm.getOrDefault(matches[i][0], 0));
         }
-        List<List<Integer>> r = Arrays.asList(new ArrayList<>(), new ArrayList<>());
-        for(Integer player : losses.keySet())
-            if(losses.get(player) <= 1)
-                r.get(losses.get(player)).add(player);
-        return r;
-        
+        ans.add(new ArrayList<>());
+        ans.add(new ArrayList<>());
+        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+            if (entry.getValue() == 0)
+                ans.get(0).add(entry.getKey());
+            else if (entry.getValue() == 1)
+                ans.get(1).add(entry.getKey());
+        }
+        Collections.sort(ans.get(0));
+        Collections.sort(ans.get(1));
+        return ans;
     }
 }
