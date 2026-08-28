f678a89d75a457baa5ac465383a9563b63f2cf2c
Khushi-Kumari006 <singhkhushi6877@gmail.com>
2026-08-28 23:00:07 +0530

    Time: 18 ms (78.8%), Space: 48.6 MB (20.4%) - LeetHub

--- 1090-largest-values-from-labels/1090-largest-values-from-labels.java
+++ 1090-largest-values-from-labels/1090-largest-values-from-labels.java
@@ -1,27 +1,39 @@
 class Solution {
-    public int largestValsFromLabels(int[] values, int[] labels, int numWanted, int useLimit) {
-        int n = values.length;
-        int arr[][]=new int[n][2];
-        int map[]=new int[20001];
-        for(int i=0;i<n;i++){
-            arr[i][0]=values[i];
-            arr[i][1]=labels[i];
-            map[labels[i]]++;
+
+    class Pair{
+        int num;
+        int label;
+        Pair(int num, int label){
+            this.num = num;
+            this.label = label;
         }
-        for(int i=0;i<n;i++){
-            map[labels[i]]=Math.min(map[labels[i]],useLimit);
+    }
+public int largestValsFromLabels(int[] val, int[] lab, int numWanted, int useLimit) {
+        HashMap<Integer,Integer> hp = new HashMap<>();
+        ArrayList<Pair> arr = new ArrayList<>();
+        for(int i=0; i<lab.length; i++){
+            arr.add(new Pair(val[i],lab[i]));
         }
-        Arrays.sort(arr,(a,b)->(a[0]-b[0]));
-        int k = n;
-        int sum=0;
-        while(--k>=0 && numWanted>0){
-            int f = map[arr[k][1]];
-            if(f>0){
-                sum+=arr[k][0];
-                map[arr[k][1]]--;
-                numWanted--;
+        Collections.sort(arr,(a,b)->(b.num-a.num));
+        int ans = 0;
+        for(int i=0; i<arr.size(); i++){
+            Pair p = arr.get(i);
+            int num = p.num;
+            int label = p.label;
+            if(numWanted>0){
+                if(!hp.containsKey(label)){
+                    ans = ans+num;
+                    hp.put(label,1);
+                    numWanted--;
+                }else{
+                    if(hp.get(label)<useLimit){
+                        ans = ans + num;
+                        hp.put(label,hp.get(label)+1);
+                        numWanted--;
+                    }
+                }
             }
         }
-        return sum;
+        return ans;
     }
 }
