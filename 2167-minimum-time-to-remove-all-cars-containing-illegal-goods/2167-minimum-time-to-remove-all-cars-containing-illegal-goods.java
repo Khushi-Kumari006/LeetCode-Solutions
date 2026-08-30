2bf6d52ee5e1d4c7cf34859654db7f79a4420779
Khushi-Kumari006 <singhkhushi6877@gmail.com>
2026-08-30 09:57:22 +0530

    Time: 556 ms (5.17%), Space: 242.5 MB (5.17%) - LeetHub

--- 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.java
+++ 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.java
@@ -1,11 +1,29 @@
 class Solution {
-    public int minimumTime(String s) {
-        int n = s.length(), left = 0, res = n;
-        for (int i = 0; i < n; ++i) {   
-            left = Math.min(left + (s.charAt(i) - '0') * 2, i + 1);
-            res = Math.min(res, left + n - 1 - i);
+    private int[][] dp;
+    private int minTime(String s, int idx, int stage) {
+        if (idx == s.length()) return 0;
+        else if(dp[idx][stage] != -1)   return dp[idx][stage];
+        int requiredTime = Integer.MAX_VALUE;
+        char curr = s.charAt(idx);
+        for (int i = stage; i < 3; i++) {
+            int temp = Integer.MAX_VALUE;
+            if (i == 0 ||  i == 2) {
+                temp = 1 + minTime(s, idx + 1, i);
+            } else if (i == 1) {
+                if (curr == '1') {
+                    temp = 2 + minTime(s, idx + 1, i);
+                } else {
+                    temp = minTime(s, idx + 1, i);
+                }
+            } 
+            requiredTime = Math.min(requiredTime, temp);
         }
-        return res;
-        
+        return dp[idx][stage] = requiredTime;
+    }
+    public int minimumTime(String s) {
+        int n = s.length()+1;
+        dp = new int[n][3];
+        for(int[] dpArr : dp)   Arrays.fill(dpArr,-1);
+        return minTime(s,0,0);
     }
 }
