61c694440547121eefbdd38030a0883670505ca0
Khushi-Kumari006 <singhkhushi6877@gmail.com>
2026-09-01 08:08:33 +0530

    Time: 37 ms (20.81%), Space: 47 MB (46.98%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
@@ -1,12 +1,18 @@
 class Solution {
     public long appealSum(String s) {
-        int last[] = new int[26];
-        Arrays.fill(last, -1);
-        long res = 0, n = s.length();
-        for (int i = 0; i < s.length(); ++i) {
-            res += (i - last[s.charAt(i) - 'a']) * (n - i);
-            last[s.charAt(i) - 'a'] = i;
+        Map<Character, Integer> map = new HashMap<>();
+        long total = 0;
+        long count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                count += i - map.get(c);
+            } else {
+                count += i + 1;
+            }
+            total += count;
+            map.put(c, i);
         }
-        return res;
+        return total;
     }
 }
