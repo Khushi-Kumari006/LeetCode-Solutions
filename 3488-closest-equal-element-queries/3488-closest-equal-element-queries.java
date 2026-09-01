fd85a48ead6e50cbe2aa7edb8440e01b2519c0dc
Khushi-Kumari006 <singhkhushi6877@gmail.com>
2026-09-01 07:54:14 +0530

    Time: 118 ms (42.34%), Space: 194.7 MB (10.51%) - LeetHub

---
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Integer> solveQueries(int[] nums, int[] queries) {
+        int n = nums.length;
+        Map<Integer, List<Integer>> indices = new HashMap<>(); 
+        int[] idxs = new int[n];  
+        for (int i = 0; i < n; i++) {
+            idxs[i] = indices.computeIfAbsent(nums[i], x -> new ArrayList<>()).size();
+            indices.get(nums[i]).add(i);
+        }
+List<Integer> result = new ArrayList<>();
+        for (int q : queries) {
+            List<Integer> qind = indices.get(nums[q]);
+            int sz = qind.size();
+            if (sz == 1) {
+                result.add(-1);
+            } else {
+                int j = idxs[q];
+                int prev = qind.get((j - 1 + sz) % sz);
+                int nextt = qind.get((j + 1) % sz);
+                result.add(Math.min((nextt + n - qind.get(j)) % n,(qind.get(j) - prev + n) % n));
+            }
+        }
+        return result;
+    }
+}
