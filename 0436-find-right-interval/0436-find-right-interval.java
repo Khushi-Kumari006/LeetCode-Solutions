f17e383ec06d329293d419c988eb210fa5444bec
Khushi-Kumari006 <singhkhushi6877@gmail.com>
2026-09-02 07:55:49 +0530

    Time: 454 ms (11.63%), Space: 53.1 MB (43.7%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.java
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -1,39 +1,19 @@
 class Solution {
-   public int[] findRightInterval(int[][] intervals) {
-        int n = intervals.length;
-        int[] arr = new int[n];
-        int[] ans = new int[n];
-        for(int i = 0; i < n; ++i)
-            arr[i] = intervals[i][0];
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < n; ++i)
-            map.put(arr[i],i);
-        Arrays.sort(arr);
-        for(int i = 0; i < n; ++i){
-            int val = binarySearch(arr,intervals[i][1]);
-            if(val == -1)
-                ans[i] = -1;
-            else
-             ans[i] = map.get(arr[val]);
-        }
-        return ans;
+    public int[] findRightInterval(int[][] intervals) {
+        int[] result = new int[intervals.length];
+        for(int i=0; i<intervals.length; i++)
+            result[i] = search(intervals[i][1], intervals);
+        return result;
     }
-    int binarySearch(int[] arr,int target){
-        int l = 0;
-        int r = arr.length - 1;
-        int ans = -1;
-        while(l <= r){
-            int mid = l + (r - l)/2;
-            if(arr[mid] == target)
-                return mid;
-            else if(arr[mid] > target){
-                ans = mid;
-                r = mid - 1;
+private int search(int num, int[][] intervals){
+        int idx = -1;
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i< intervals.length; i++){
+            if(intervals[i][0] >= num && intervals[i][0] < min){
+                idx = i;
+                min = intervals[i][0];
             }
-            else
-                l = mid + 1;
         }
-        return ans;
+        return idx;
     }
 }
-        
