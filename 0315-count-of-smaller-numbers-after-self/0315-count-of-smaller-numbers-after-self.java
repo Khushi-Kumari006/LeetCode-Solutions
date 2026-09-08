d9950755ed1e40c4c9d725a0600f1f072056f985
Khushi-Kumari006 <singhkhushi6877@gmail.com>
2026-09-08 22:27:21 +0530

    Time: 18 ms (97.96%), Space: 95.8 MB (74.36%) - LeetHub

---
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
@@ -0,0 +1,29 @@
+
+class Solution {
+    int[] bit = new int[20002];
+      void add(int i) {
+        while (i < bit.length) {
+            bit[i]++;
+            i += i & -i;
+        }
+    }
+    int sum(int i) {
+        int s = 0;
+        while (i > 0) {
+            s += bit[i];
+            i -= i & -i;
+        }
+        return s;
+    }
+    public List<Integer> countSmaller(int[] nums) {
+        int n = nums.length;
+        Integer[] ans = new Integer[n];
+        for (int i = n - 1; i >= 0; i--) {
+            int x = nums[i] + 10001;
+
+            ans[i] = sum(x);
+            add(x + 1);
+        }
+        return Arrays.asList(ans);
+    }
+}
