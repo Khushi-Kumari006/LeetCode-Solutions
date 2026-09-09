cccdb4363de17d47f98227da44a39dcc021b011f
Khushi-Kumari006 <singhkhushi6877@gmail.com>
2026-09-09 08:20:47 +0530

    Time: 1 ms (99.36%), Space: 42.6 MB (71.15%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,8 +1,12 @@
 class Solution {
-    private static final long[] pow1000 = { 1000L, 1000000L, 1000000000L, 1000000000000L, 1000000000000000L, 1000000000000000000L };    
     public long countCommas(long n) {
-        int k = 0;        
-        for (long p : pow1000) if (n >= p) k++;
-        return k * (n + 1) - (pow1000[k] - 1000) / 999;
+        if (n <= 999) return 0;
+        long totalCommas = 0;
+        long start = 1000;
+        while (start <= n) {
+            totalCommas += n - start + 1;
+            start *= 1000;
+        }
+        return totalCommas;
     }
 }
