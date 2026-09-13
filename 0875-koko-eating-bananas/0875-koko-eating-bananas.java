dd3818c98dabf20a9b732c9ffbbb068ef3b7e5b7
Khushi-Kumari006 <singhkhushi6877@gmail.com>
2026-09-13 08:30:27 +0530

    Time: 13 ms (40.97%), Space: 47.7 MB (87.42%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,25 +1,25 @@
-public class Solution {
+class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int left = 1;  
-        int right = Arrays.stream(piles).max().getAsInt();
-        int ans = right;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (canFinish(piles, h, mid)) {
-                ans = mid;     
-                right = mid - 1;
+        int low = 1;
+        int high = Integer.MIN_VALUE;
+        for (int i = 0; i < piles.length; i++) {
+            high = Math.max(high, piles[i]);
+        }
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long hours = 0;
+            for (int i = 0; i < piles.length; i++) {
+                hours += piles[i] / mid;
+                if (piles[i] % mid != 0) {
+                    hours++;
+                }
+            }
+            if (hours <= h) {
+                high = mid - 1;
             } else {
-                left = mid + 1; 
+                low = mid + 1;
             }
         }
-        return ans;
-    }
-    public boolean canFinish(int[] piles, int h, int k) {
-        long hours = 0;
-        for (int pile : piles) {
-            hours += pile / k;
-            if (pile % k != 0) hours++;
-        }
-        return hours <= h;
+        return low;
     }
 }
