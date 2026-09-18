dfc72756810dd3aa1470184718fcf25b79ae134d
Khushi-Kumari006 <singhkhushi6877@gmail.com>
2026-09-18 14:04:23 +0530

    Time: 28 ms (17.4%), Space: 67.8 MB (10.32%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.java
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.java
@@ -1,17 +1,36 @@
 class Solution {
-    public int shortestSubarray(int[] A, int K) {
-        int N = A.length, res = N + 1;
-        long[] B = new long[N + 1];
-        for (int i = 0; i < N; i++) B[i + 1] = B[i] + A[i];
-        Deque<Integer> d = new ArrayDeque<>();
-        for (int i = 0; i < N + 1; i++) {
-            while (d.size() > 0 && B[i] - B[d.getFirst()] >=  K)
-                res = Math.min(res, i - d.pollFirst());
-            while (d.size() > 0 && B[i] <= B[d.getLast()])
-                d.pollLast();
-            d.addLast(i);
+    public int shortestSubarray(int[] nums, int k) {
+        int res = Integer.MAX_VALUE;
+        long curSum = 0;
+        Deque<Pair<Long, Integer>> q = new ArrayDeque<>();        
+        for (int r = 0; r < nums.length; r++) {
+            curSum += nums[r];
+            if (curSum >= k) {
+                res = Math.min(res, r + 1);
+            }
+            while (!q.isEmpty() && curSum - q.peekFirst().getKey() >= k) {
+                Pair<Long, Integer> front = q.pollFirst();
+                res = Math.min(res, r - front.getValue());
+            }
+            while (!q.isEmpty() && q.peekLast().getKey() > curSum) {
+                q.pollLast();
+            }
+            q.offerLast(new Pair<>(curSum, r));
+        }        
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+    static class Pair<K, V> {
+        private K key;
+        private V value;        
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
         }
-        return res <= N ? res : -1;
-        
+        public K getKey() {
+            return key;
+        }
+        public V getValue() {
+            return value;
+        }
     }
 }
