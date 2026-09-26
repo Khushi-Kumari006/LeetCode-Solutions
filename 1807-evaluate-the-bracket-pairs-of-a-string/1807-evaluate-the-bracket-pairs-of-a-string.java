fd2700d67ab141e97cb1d7192df99ba739228ac9
Khushi-Kumari006 <singhkhushi6877@gmail.com>
2026-09-26 13:24:19 +0530

    Time: 33 ms (85.52%), Space: 91 MB (62.9%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -1,28 +1,26 @@
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
-        Map<String, String> map = new HashMap<>();
-        for (List<String> pair : knowledge) {
-            map.put(pair.get(0), pair.get(1));
+        Map<String, String> d = new HashMap<>();
+        for (List<String> item : knowledge) {
+            d.put(item.get(0), item.get(1));
         }
-        StringBuilder result = new StringBuilder();
-        StringBuilder key = new StringBuilder();
-        boolean inBracket = false;
+        StringBuilder ans = new StringBuilder();
+        int start = -1;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == '(') {
-                inBracket = true;
-                key.setLength(0);
-            } else if (c == ')') {
-                inBracket = false;
-                result.append(map.getOrDefault(key.toString(), "?"));
-            } else {
-                if (inBracket) {
-                    key.append(c);
-                } else {
-                    result.append(c);
-                }
+                start = i;
+            }
+            else if (c == ')') {
+                String key = s.substring(start + 1, i);
+
+                ans.append(d.getOrDefault(key, "?"));
+                start = -1;
+            }
+            else if (start < 0) {
+                ans.append(c);
             }
         }
-        return result.toString();
+        return ans.toString();
     }
 }
